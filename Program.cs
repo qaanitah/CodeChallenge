9c7141a951c68de3183ba57bb50931a9d87f2a6b
qaanitah <qaanitahoyekola@gmail.com>
2022-08-30 14:06:44 -0700

    CodeChallenge

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int commonChild (string s1, string s2);
+            int counter = 0;
+            for(int i = 0; i < s1.Length; i++)
+            {
+                for(int k = 0; k < s2.Length; k++)
+                {
+                    if (s1[i] == s2[k])
+                    {
+                        s2 = s2.Remove[k, i]
+                        counter ++;
+                        break;
+                    }
+                }
+            }
+        }
+        public static int StudentsCandy()
+        {
+            int n = 0;
+            int [] arr = [4, 6, 4, 5, 6, 2];
+            int [] minAmount = [1, 2, 1, 2, 3, 1];
+            for(int i = 0; i < arr.Length; i++)
+        }
+    }
+}
